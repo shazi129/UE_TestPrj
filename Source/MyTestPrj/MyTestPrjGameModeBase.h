fb93801e1d7c04354969c846422fe2c62e84b7bb
shazi129 <apply_count@sina.com>
2020-11-06 16:35:30 +0800

    TestShaderPlugin

---
+++ Source/MyTestPrj/MyTestPrjGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyTestPrjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTESTPRJ_API AMyTestPrjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
